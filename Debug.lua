3e9d4740669b3fdccb6cb84799af37299343d520
lieyanatotswear-create <lieyanatotswear@gmail.com>
2025-09-02 10:22:29 +0800

    Debug.lua

--- Debug.lua
+++ Debug.lua
@@ -1,1 +1,6 @@
-print("✅️ Debug: Script berjaya load dari GitHub!")
+local sg = Instance.new("ScreenGui")
+sg.Parent = game.Players.LocalPlayer.PlayerGui
+local f = Instance.new("Frame", sg)
+f.Size = UDim2.new(0, 100, 0, 100)
+f.Position = UDim2.new(0.5,-50, 0.5,-50)
+f.BackgroundColor3 = Color3.fromRGB(255,0,0)
